46c5b0f660d7ba0b90af4a2ae2b38279534eb55d
Matt Gallagher <mattgallagher@users.noreply.github.com>
2024-12-01 17:48:05 +1100

    Initial Commit

---
+++ ToneGenerator/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ToneGenerator
+//
+//  Created by Matthew Gallagher on 1/12/2024.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ToneGenerator/ToneGeneratorApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToneGeneratorApp.swift
+//  ToneGenerator
+//
+//  Created by Matthew Gallagher on 1/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToneGeneratorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
